4eb9cd27f78e61193d68e09e2c9200675c10eb6f
Pooja Saraf <pooja.saraf12@gmail.com>
2024-09-06 00:20:21 +0530

    Update list-users.sh
    helper function added.

--- list-users.sh
+++ list-users.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+helper()
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -36,6 +37,13 @@ function list_users_with_read_access {
     fi
 }
 
+function helper{
+expected_cmd_args=2
+if [ #$ -ne $exppected_amd_args]; then
+echo "please execute the script with required arguments"
+echo "organisation owner and repository name is required "
+fi
+}
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
